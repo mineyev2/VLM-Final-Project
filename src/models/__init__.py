5c1fbcad6ef4339e9b404cd333960d8dc89f3fdb
Roman Igorevich Mineyev <rmineyev3@login-ice-1.pace.gatech.edu>
2025-11-17 15:41:49 -0500

    evaluate_v3.py complete

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 # Models package
-from .lidarclip_qwen_model import LidarCLIPQwenModel
-from .sst import LidarEncoderSST
+# from .lidarclip_qwen_model import LidarCLIPQwenModel
+# from .sst import LidarEncoderSST
 
-__all__ = ['LidarCLIPQwenModel', 'LidarEncoderSST']
+# __all__ = ['LidarCLIPQwenModel', 'LidarEncoderSST']
